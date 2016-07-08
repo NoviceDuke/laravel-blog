8d02c6e3692c0b4d3f7788974fe94f4c5fbe2ed4
g9308370 <g9308370@hotmail.com>
2016-07-08 17:44:52 +0800

    hchs branch

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,7 +39,7 @@
         <div class="container">
             <div class="content">
 
-                <div class="title">Laravel 5 Mcunoobs 123</div>
+                <div class="title">Laravel 5 hchs sss</div>
 
                 
             </div>
