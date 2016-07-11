8323dea765eedfc89562af024181e2c368e48412
g9308370 <g9308370@hotmail.com>
2016-07-12 00:18:46 +0800

    edit Card

--- resources/views/hchs/index.blade.php
+++ resources/views/hchs/index.blade.php
@@ -1,26 +1,38 @@
 @extends('hchs/app')
 @section('content')
    
-       <div class="container">
+			<div class="container">
              <div class="row">
 			      <div class="col s8">
-					  <div class="card medium">
-					    <div class="card-image waves-effect waves-block waves-light">
+						<!--Card-->
+					    <div class="card medium hoverable">
+					      <div class="card-image waves-effect waves-block waves-light">
 					      <img class="activator" src="http://materializecss.com/images/sample-1.jpg">
-					    </div>
-					    <div class="card-content">
+					      </div>
+					      <div class="card-content">
 					      <span class="card-title  grey-text text-darken-4 ">Card Title
 					      <i class="card-title material-icons right activator">more_vert</i></span>
 					      
-					    </div>
-					    <div class="card-reveal">
+					      </div>
+					      <div class="card-reveal">
 					      <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
 					      <p>Here is some more information about this product that is only revealed once clicked on.</p>
 					    </div>
-					  </div>
+					    </div>
+					    <!--Card-->
 			      </div>
-			      <div class="col s4">This div is 4444444444444-columns wide</div>
+			      <div class="col s4">
+			      	    <div class="card medium hoverable">
+			      	    	 <div class="collection">
+							    <a href="#!" class="collection-item">Alan<span class="badge">1</span></a>
+							    <a href="#!" class="collection-item">Alan<span class="new badge">4</span></a>
+							    <a href="#!" class="collection-item">Alan</a>
+							    <a href="#!" class="collection-item">Alan<span class="badge">14</span></a>
+							 </div>
+			      	    </div>
+			      </div>
 			 </div>
-       </div>
+			 </div>
+
 
 @endsection
