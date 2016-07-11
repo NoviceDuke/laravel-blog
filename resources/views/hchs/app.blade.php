546df6e4d491f73e0ec5892296791b84ac10a531
g9308370 <g9308370@hotmail.com>
2016-07-11 17:21:07 +0800

    app.blade.php add Floating button Navigationbar

--- resources/views/hchs/app.blade.php
+++ resources/views/hchs/app.blade.php
@@ -2,13 +2,45 @@
 <html>
     <head>
         <title>HCHS Blog Trace Page</title>
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css">
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
+        <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+        {!! Html::script('https://code.jquery.com/jquery-2.1.1.min.js')!!}
+        {!! Html::script('materialize/js/materialize.js')!!}
+        {!! Html::style('materialize/css/materialize.css')!!}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     </head>
     <body>
-     <div class="container">
+        <!--  Top Navigation  -->
+		<nav>
+		  <div class="nav-wrapper">
+		    <a href="" class="brand-logo">　Blog Using Material Design Trace</a>
+		    <ul id="nav-mobile" class="right hide-on-med-and-down">
+		      <li><a href="">item1</a></li>
+		      <li><a href="">item2</a></li>
+		      <li><a href="">item3<span class="new badge">4</span></a></li>
+		      
+		    </ul>
+		  </div>
+		</nav>
+		<!--  Top Navigation  -->
+
+
+        <!--  Floating Button -->
+        <div class="fixed-action-btn" style="bottom: 45px; right: 24px;">
+		    <a class="btn-floating btn-large red">
+		      <i class="large material-icons">mode_edit</i>
+		    </a>
+		    <ul>
+		      <li><a class="btn-floating red"><i class="material-icons">insert_chart</i></a></li>
+		      <li><a class="btn-floating yellow darken-1"><i class="material-icons">format_quote</i></a></li>
+		      <li><a class="btn-floating green"><i class="material-icons">publish</i></a></li>
+		      <li><a class="btn-floating blue"><i class="material-icons">attach_file</i></a></li>
+		    </ul>
+    	</div>
+    	<!--  Floating Button -->
+
+
          @yield('content')
+         
      </div>
     </body>
 </html>
--- resources/views/hchs/index.blade.php
+++ resources/views/hchs/index.blade.php
@@ -1,19 +1,6 @@
 @extends('hchs/app')
 @section('content')
    
-     <div class="row">
-      <div class="col s1">1</div>
-      <div class="col s1">2</div>
-      <div class="col s1">3</div>
-      <div class="col s1">4</div>
-      <div class="col s1">5</div>
-      <div class="col s1">6</div>
-      <div class="col s1">7</div>
-      <div class="col s1">8</div>
-      <div class="col s1">9</div>
-      <div class="col s1">10</div>
-      <div class="col s1">11</div>
-      <div class="col s1">12</div>
-    </div>
+      
 
 @endsection
