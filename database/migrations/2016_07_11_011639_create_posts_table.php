3e46cdfcabeeb5b2c37459319d4805f7faa6a61b
g9308370 <g9308370@hotmail.com>
2016-07-11 09:46:48 +0800

    1.建立ORM Model Post   app/Post.php 2.建立Migration posts_table   database/migrations/2016******create_posts_table.php 3.建立Seeder PostSeeder   database/seeds/PostSeeder.php   同時更改DatabaseSeeder.php

---
+++ database/migrations/2016_07_11_011639_create_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+         Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('author_email');
+            $table->text('title');
+            $table->text('content');
+            $table->string('tags')->nullable();
+            $table->string('pic_url')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('posts');
+    }
+}
---
+++ database/seeds/PostSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Post;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('posts')->delete();
+        Post::create([
+
+      	 'author_email'	   	=> 		'g9308370@hotmail.com',
+      	 'title'            =>      'Seeder自動建立標題',
+         'content'          =>      'asdwqsxxxxmmmmm!!!!!我在測試Seeder自動建立content',
+         'tags'             => 		'這裡應該使用Array，但是我不會用',
+         'pic_url'          => 		'www.google.com',
+        	    
+      ]);
+    }
+}
