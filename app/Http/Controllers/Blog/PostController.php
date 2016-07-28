ecf096bf4674283c1b66b1800154cefeca6f172b
g9308370 <g9308370@hotmail.com>
2016-07-28 21:34:50 +0800

    remove edit

--- app/Http/Controllers/Blog/PostController.php
+++ app/Http/Controllers/Blog/PostController.php
@@ -42,16 +42,6 @@ class PostController extends Controller
     }
 
     /**
-     *   show the post create view.
-     */
-    public function edit($slug)
-    {
-        $titlew=where
-
-        return view(edit);
-    }
-
-    /**
      *   store a new post .
      */
     public function store(Request $request)
