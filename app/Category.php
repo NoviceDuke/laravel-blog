ab7e7b81b373eafb10bc4e30bc4242f32f1b19e8
g9308370 <g9308370@hotmail.com>
2016-07-29 21:06:37 +0800

    1.加入新的資料庫關聯 文章種類    文章種類(1) 對 (*)文章 2.修正一些檔案順序
     	new file:   app/Category.php
     	modified:   app/Post.php
     	new file:   database/migrations/2016_07_28_124129_create_categories_table.php
     	renamed:    database/migrations/2016_07_11_011639_create_posts_table.php -> database/migrations/2016_07_29_011639_create_posts_table.php
     	renamed:    database/migrations/2016_07_12_081650_create_comments_table.php -> database/migrations/2016_07_29_081650_create_comments_table.php

---
+++ app/Category.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    protected $fillable = [
+            'name',         //種類名稱
+    ];
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    public function addPost(Post $post)
+    {
+        return $this->posts()->save($post);
+    }
+}
