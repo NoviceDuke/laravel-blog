b3fcb6e6ef7c9c9da4d8e9e540239331ed5b297f
g9308370 <g9308370@hotmail.com>
2016-08-04 21:08:56 +0800

    新增一個Search的樣式

--- resources/views/blog/rightSide.blade.php
+++ resources/views/blog/rightSide.blade.php
@@ -1,11 +1,31 @@
-
-<div class="category-container">
-	<h3 class="category-container-title title-fix">Categories</h3>
-	<div class="collection collection-fix">
-		<a href="#!" class="collection-item border-right-red grey-text">{{$categories[0]->name}}</a>
-		<a href="#!" class="collection-item border-right-blue grey-text">{{$categories[1]->name}}</a>
-		<a href="#!" class="collection-item border-right-yellow grey-text">{{$categories[2]->name}}</a>
-		<a href="#!" class="collection-item border-right-green grey-text">{{$categories[3]->name}}</a>
-		<a href="#!" class="collection-item border-right-purple grey-text">{{$categories[4]->name}}</a>
+<!-- Search -->
+<div class="row">
+  	<div class="col s12 m12">
+		<div class="category-container">
+		<h3 class="category-container-title title-fix">Search</h3>
+			<div class="row">
+			  <div class="input-field col s12 m12">
+				<i class="material-icons prefix search-icon-fix">search</i>
+				<input class="search-input" type="text" id="autocomplete">
+			  </div>
+			</div>
+		</div>
 	</div>
 </div>
+<!-- Search -->
+<!-- Categories -->
+<div class="row">
+	<div class="col s12 m12">
+		<div class="category-container">
+			<h3 class="category-container-title title-fix">Categories</h3>
+			<div class="collection collection-fix">
+				<a href="#!" class="collection-item border-right-red grey-text">{{$categories[0]->name}}</a>
+				<a href="#!" class="collection-item border-right-blue grey-text">{{$categories[1]->name}}</a>
+				<a href="#!" class="collection-item border-right-yellow grey-text">{{$categories[2]->name}}</a>
+				<a href="#!" class="collection-item border-right-green grey-text">{{$categories[3]->name}}</a>
+				<a href="#!" class="collection-item border-right-purple grey-text">{{$categories[4]->name}}</a>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- Categories -->
