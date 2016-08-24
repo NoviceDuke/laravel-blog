137e8f4cc18160d29b876ceb469fcfe2ce88a5b5
g9308370 <g9308370@hotmail.com>
2016-08-24 23:01:42 +0800

    單元測試 : Models: Article

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -19,3 +19,27 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Articles\Article::class, function (Faker\Generator $faker) {
+    return [
+        'title' => $faker->sentence,
+        'content' => $faker->realText($maxNbChars = 200, $indexSize = 2),
+        'slug' => str_slug($faker->sentence, '-'),
+    ];
+});
+
+$factory->define(App\Articles\Comment::class, function (Faker\Generator $faker) {
+    return [
+        'content' => $faker->realText($maxNbChars = 200, $indexSize = 2),
+        'status' => '未讀',
+        'author' => $faker->name,
+        'url' => $faker->url,
+        'email' => $faker->safeEmail,
+    ];
+});
+$factory->define(App\Articles\Tag::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->word,
+        'frequency' => 0,
+    ];
+});
