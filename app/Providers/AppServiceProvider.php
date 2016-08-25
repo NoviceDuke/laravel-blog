7aef21aa2dadf511796a51faf83d0c1428f8a4f2
NoviceDuke <duke00360753@gmail.com>
2016-08-25 20:28:41 +0800

    1. add test enviroment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,23 +4,20 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
-class AppServiceProvider extends ServiceProvider
-{
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot()
-    {
-        //
-    }
+class AppServiceProvider extends ServiceProvider {
+	/**
+	 * Bootstrap any application services.
+	 */
+	public function boot() {
+		//
+	}
 
-    /**
-     * Register any application services.
-     */
-    public function register()
-    {
-        if ($this->app->environment() == 'local-hchs') {
-            $this->app->register('Barryvdh\Debugbar\ServiceProvider');
-        }
-    }
+	/**
+	 * Register any application services.
+	 */
+	public function register() {
+		if ($this->app->environment() == 'local-hchs' || $this->app->environment() == 'local-duke') {
+			$this->app->register('Barryvdh\Debugbar\ServiceProvider');
+		}
+	}
 }
