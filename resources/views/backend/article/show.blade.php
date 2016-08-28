8e43358ee692d0338fb43c017faf78d771efea3b
NoviceDuke <duke00360753@gmail.com>
2016-08-28 18:02:59 +0800

    1.add fucking idiot btn and time list

--- resources/views/backend/article/show.blade.php
+++ resources/views/backend/article/show.blade.php
@@ -8,6 +8,35 @@
         <h1>{{$articles->title}}</h1>
         <p>{{$articles->content}}</p>
       </div>
+      <div class="col s4">
+        <div class="well">
+          <dl class="dl-horizontal">
+            <dt>
+              Created At:
+            </dt>
+            <dd>
+              {{$articles->created_at}}
+            </dd>
+          </dl>
+          <dl class="dl-horizontal">
+            <dt>
+              Updated At:
+            </dt>
+            <dd>
+              {{$articles->update_at}}
+            </dd>
+          </dl>
+          <hr />
+          <div class="row">
+            <div class="col s4">
+              {!! Html::linkRoute('backend.article.show','Canceal',array($articles->id),array('class'=>'btn btn-danger waves-effect waves-light btn'))!!}
+            </div>
+            <div class="col s4">
+              {!! Html::linkRoute('backend.article.update','Save',array($articles->id),array('class'=>'btn btn-success waves-effect waves-light btn'))!!}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
 
 
