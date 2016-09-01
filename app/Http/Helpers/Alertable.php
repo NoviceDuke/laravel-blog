f1d9fc92838e8f224aea48572820b597d79cbcdf
g9308370 <g9308370@hotmail.com>
2016-09-01 22:41:44 +0800

    1. 完成sweet_alert~~ 2. 寫了一個Helpe Trait在 App\Http\Helpers\Alertable.php -只需要在Controller use，就能使用global function -詳細功能在 App\Http\Helpers\Alertable.php --------------------------------------------------------------------  	modified:   app/Http/Controllers/Blog/ArticleController.php  	modified:   app/Http/Helpers/Alertable.php  	modified:   gulpfile.js  	new file:   public/css/libs.css  	new file:   public/css/libs.css.map  	new file:   public/js/libs.js  	new file:   public/js/libs.js.map  	new file:   resources/assets/js/libs/prism.js  	new file:   resources/assets/js/libs/sweetalert2.js  	new file:   resources/assets/sass/libs/prism.scss  	new file:   resources/assets/sass/libs/sweetalert2.scss  	modified:   resources/views/blog/app.blade.php  	modified:   resources/views/blog/article/show.blade.php  	new file:   resources/views/partials/sweet_alert.blade.php  	new file:   tests/units/features/AlertableTest.php

--- app/Http/Helpers/Alertable.php
+++ app/Http/Helpers/Alertable.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Http\Helpers;
 
 /**
@@ -8,9 +9,62 @@ namespace App\Http\Helpers;
  */
 trait Alertable
 {
+    private $key = 'alert_message';
+    private $title = 'default title';
+    private $message = 'Hi Im Default Alert!';
+    private $level = 'info';
 
-    function allert($message)
+    /**
+     * 初始化SweetAllert.
+     *
+     * 可選擇傳進標題、內容資訊，填入null表示該變數使用預設值
+     *
+     * @return $this
+     */
+    public function alert($title = null, $message = null)
     {
+        $this->title = $title ?: $this->title;
+        $this->message = $message ?: $this->message;
 
+        return $this;
+    }
+
+    /**
+     * 將變數Flash存進Session Defaul key = alert_message.
+     *
+     * 可以選擇傳進Arg指定Session key;
+     */
+    public function flashIt($key = null)
+    {
+        $this->key = $key ?: $this->key;
+
+        return session()->flash($this->key, [
+            'title' => $this->title,
+            'message' => $this->message,
+            'level' => $this->level,
+        ]);
+    }
+
+    /**
+     * 魔術方法.
+     *
+     * 可以使用method name指定其level值
+     * 目前SweetAllert支援 : "warning", "error", "success" and "info"
+     */
+    public function __call($level, $args)
+    {
+        $support = [
+            'warning',
+            'error',
+            'success',
+            'info',
+        ];
+        if (in_array($level, $support)) {
+            $this->level = $level;
+        } else {
+            throw new \Exception("Error SweetAllert Type $level");
+        }
+
+        return $this;
     }
 }
--- gulpfile.js
+++ gulpfile.js
@@ -14,10 +14,26 @@ var elixir = require('laravel-elixir');
 elixir(function(mix) {
     // original elixir mix sass. I don't need this  sass(bootstrap)
     // mix.sass('app.scss');
-    mix.sass('blog-styles.scss','public/css/blog-styles.css');
+
+    // 自己的scss與js
+    mix.sass('blog-styles.scss', 'public/css/blog-styles.css');
     mix.scripts([
         'blog-styles.js',
-    ],'public/js/blog-styles.js');
+    ], 'public/js/blog-styles.js');
+
+
+    // 別人的libs
+    mix.sass([
+        'libs/prism.scss',
+        'libs/sweetalert2.scss',
+    ], 'public/css/libs.css');
+
+    mix.scripts([
+        '/libs/prism.js',
+        '/libs/sweetalert2.js',
+    ], 'public/js/libs.js');
+
+
     mix.browserSync({
         proxy: 'localhost/laravel-blog/public'
     });
