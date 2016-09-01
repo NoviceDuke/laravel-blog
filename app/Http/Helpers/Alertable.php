ac2f90c8fea4dbb0d7115e6c0d12c7c55752fa55
g9308370 <g9308370@hotmail.com>
2016-09-01 22:46:41 +0800

    1. 補注解 2. Alertable 的單元˙測試也完成了

--- app/Http/Helpers/Alertable.php
+++ app/Http/Helpers/Alertable.php
@@ -6,6 +6,10 @@ namespace App\Http\Helpers;
  * 讓Controller能夠直接使用Alert.
  *
  * 主要是用Seesion的方式存入變數，在Blade內判斷是否存有指定的Session，並做適當輸出.
+ *
+ * 基本用法
+ * $this->alert()->flashIt();
+ * 這樣就會產生一個key為alert_message的Session
  */
 trait Alertable
 {
