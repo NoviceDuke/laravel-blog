00cac2efcb61a585cd64a629081816d5c0e6ce4d
Hchs <g9308370@hotmail.com>
2016-10-29 17:38:13 +0800

    try to hardcode to animate

--- resources/views/blog/layouts/app.blade.php
+++ resources/views/blog/layouts/app.blade.php
@@ -17,12 +17,35 @@
         </script>
         <!--Scripts End-->
     </head>
+    <style>
+    .my-loading-nav {
+        width: 100px;
+        height: 100px;
+        background-color: red;
+        -webkit-animation-name: example; /* Safari 4.0 - 8.0 */
+        -webkit-animation-duration: 4s; /* Safari 4.0 - 8.0 */
+        animation-name: example;
+        animation-duration: 4s;
+    }
+
+    /* Safari 4.0 - 8.0 */
+    @-webkit-keyframes example {
+        from {background-color: red;}
+        to {background-color: yellow;}
+    }
+
+    /* Standard syntax */
+    @keyframes example {
+        from {background-color: red;}
+        to {background-color: yellow;}
+    }
+    </style>
     @inject('authPresenter', 'App\Presenters\AuthPresenter')
     @inject('floatingButtonPresenter', 'App\Presenters\FloatingButtonPresenter')
     <body background="{{url('png/hchs_background.png')}}">
         <span id="app">
         <!--  Top Navigation  -->
-        <div class=" loading-nav">
+        <div class="my-loading-nav">
 
         </div>
         {{-- <nav class="blue-grey darken-2 nav-height-fix loading-nav"></nav> --}}
