78b78d35511d08556c3035812b09c9dd7740e4c6
Duke <duke00360753@gmail.com>
2016-10-31 04:00:46 +0800

    1.Beautify backend_article_show

--- resources/views/backend/article/show.blade.php
+++ resources/views/backend/article/show.blade.php
@@ -2,37 +2,49 @@
 
 @section('title','| Article')
 @section('content')
-  <div >
-    <div class="row">
-      <div class="col-xs-12 col-sm-6 col-lg-8">
-        <h1>{{$articles->title}}</h1>
-        <p>{{$articles->content}}</p>
+  <div class="panel panel-success">
+    <div class="panel panel-heading">
+      <h3 class="panel-title">
+        <div class="row">
+          <div class="text-center">
+            {{$articles->title}}
+          </div>
+        </div>
+      </h3>
+    </div>
+
+
+    <div class="panel panel-body">
+
+      <p>{{$articles->content}}</p>
+    </div>
+    <div class="panel panel-footer">
+      <div class="row">
+        <div class="col-md-6 text-center">
+          發表於 {{$articles->created_at}}
+        </div>
+          <div class="col-md-6 text-center">
+            修改於 {{$articles->updated_at}}
+          </div>
+
+
       </div>
-      <div class="col-xs-6 col-lg-7">
-        <div class="well">
-          <dl class="dl-horizontal">
-            <dt>
-              Created At:
-            </dt>
-            <dd>
-              {{$articles->created_at}}
-            </dd>
-          </dl>
-          <dl class="dl-horizontal">
-            <dt>
-              Updated At:
-            </dt>
-            <dd>
-              {{$articles->updated_at}}
-            </dd>
-          </dl>
-          <hr />
+
+    </div>
+  </div>
+
+
           <div class="row">
             <div class="col-md-4">
-              {!! Html::linkRoute('backend.article.edit','Edit',array($articles->id),array('class'=>'btn btn-danger waves-effect waves-light btn red'))!!}
+              <a href="/backend/article/{{$articles->id}}" class="btn btn-danger">Edit</a>
+              <!--{!! Html::linkRoute('backend.article.edit','Edit',array($articles->id),array('class'=>'btn btn-danger '))!!}
+              -->
             </div>
             <div class="col-md-4">
-              {!! Html::linkRoute('backend.article.index','Back',array($articles->id),array('class'=>'btn btn-success waves-effect waves-light btn'))!!}
+              <a href="/backend/article" class="btn btn-success">Back</a>
+              <!--
+              {!! Html::linkRoute('backend.article.index','Back',array($articles->id),array('class'=>'btn btn-success '))!!}
+            -->
             </div>
           </div>
         </div>
